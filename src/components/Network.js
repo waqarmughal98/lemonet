fe615daab5ed9829b110ccef2c282e0d2110d067
Waqar Ali <52863555+waqarmughal98@users.noreply.github.com>
2022-08-16 13:11:35 +0500

    up

--- src/components/Network.js
+++ src/components/Network.js
@@ -22,63 +22,57 @@ const Network = () => {
           </p>
         </div>
 
-        <div className="swipe-div justify-center mx-auto py-12">
-          <Swiper
-            spaceBetween={10}
-            slidesPerView={2}
-            onSlideChange={() => console.log("slide change")}
-            onSwiper={(swiper) => console.log(swiper)}
-          >
-            <SwiperSlide>
-              <div className="relative p-8 bg-white text-left z-10 drop-shadow-xl">
-                <h1 className="uppercase text-para font-bold text-3xl">
-                  Problem
-                </h1>
+        <div className="swipe-div justify-center mx-auto py-32 grid grid-cols-2 gap-6">
+          <div className="relative py-20 pl-12 pr-40 w-[600px] bg-white text-left z-10 drop-shadow-xl">
+            <h1 className="uppercase text-para font-bold opacity-40 text-3xl">
+              Problem
+            </h1>
 
-                <h1 className="text-primary font-secondary text-xl pt-4 pb-2">
-                  Conventional Social Networks
-                </h1>
+            <h1 className="text-primary font-secondary text-xl pt-4 pb-2">
+              Conventional Social Networks
+            </h1>
 
-                <ul className="list-disc list-inside marker:text-primary space-y-2">
-                  <li>
-                    Sed do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua.
-                  </li>
-                  <li>Wende omnis iste natus error sit volupt.</li>
-                  <li>
-                    Quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-                    ea commodo consequat.
-                  </li>
-                </ul>
-              </div>
-            </SwiperSlide>
+            <ul className="list-disc list-inside marker:text-primary space-y-2">
+              <li>
+                Sed do eiusmod tempor incididunt ut labore et dolore magna
+                aliqua.
+              </li>
+              <li>Wende omnis iste natus error sit volupt.</li>
+              <li>
+                Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                commodo consequat.
+              </li>
+            </ul>
+          </div>
 
-            <SwiperSlide>
-              <div className="relative p-8 bg-white text-left z-10 drop-shadow-xl">
-                <h1 className="uppercase text-para font-bold text-3xl">
-                  Problem
-                </h1>
+          <div className="relative">
+            <div className="absolute -left-12 -top-12 py-20 pl-12 pr-40 w-[600px] bg-primary text-left z-10 drop-shadow-xl">
+              <h1 className="uppercase text-white font-bold text-3xl">
+                Solution
+              </h1>
 
-                <h1 className="text-primary font-secondary text-xl pt-4 pb-2">
-                  Conventional Social Networks
-                </h1>
+              <h1 className="text-white font-secondary text-xl pt-4 pb-2">
+                Decentralized Social Networks
+              </h1>
 
-                <ul className="list-disc list-inside marker:text-primary space-y-2">
-                  <li>
-                    Sed do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua.
-                  </li>
-                  <li>Wende omnis iste natus error sit volupt.</li>
-                  <li>
-                    Quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-                    ea commodo consequat.
-                  </li>
-                </ul>
-              </div>
-            </SwiperSlide>
-            <div class="swiper-button-prev"></div>
-            <div class="swiper-button-next"></div>
-          </Swiper>
+              <ul className="list-disc list-inside text-white marker:text-secondary space-y-2">
+                <li>
+                  Sed do eiusmod tempor incididunt ut labore et dolore magna
+                  aliqua.
+                </li>
+                <li>Wende omnis iste natus error sit volupt.</li>
+                <li>
+                  Quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+                  ea commodo consequat.
+                </li>
+              </ul>
+            </div>
+            <img
+              src="./images/network-right.png"
+              className="absolute right-0 bottom-0"
+              alt=""
+            />
+          </div>
         </div>
       </div>
     </div>
